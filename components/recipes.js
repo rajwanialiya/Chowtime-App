94598030d5d5568c50f2fc8e3b136d144667874e
Aliya Rajwani <rajwani.aliya@gmail.com>
2020-05-13 21:13:19 -0400

    formatting

--- components/recipes.js
+++ components/recipes.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
-import { ActivityIndicator, FlatList} from 'react-native';
-import { global } from '../styles'
-import { Button, Text, ThemeProvider, View } from 'react-native-elements';
+import React, { useState }from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Text, ActivityIndicator } from 'react-native-paper';
+
+//Theme
+import { global } from '../styles';
+
+//Styles
+import { view } from '../styles'
+import { title } from '../styles'
 
 export function Recipes() {
   const [isLoading, setLoading] = useState(true);
@@ -18,30 +25,44 @@ export function Recipes() {
     + '&app_key=' + appKey
 
 
-  if (isLoading) { //IS THIS PROPER??
+  if (isLoading) {
     fetch(url)
       .then((response) => response.json())
       .then((json) => setData(json.hits))
       .catch((error) => console.error('oh no')) //figure out how to display error 
       .finally(() => setLoading(false));
     
-      return <ActivityIndicator/>
+      return (
+        <View style={styles.view}>
+          <ActivityIndicator 
+            color='#32CA81'
+            size='large'
+          />
+        </View>
+      )
   } else {
     return (
-      <ThemeProvider theme={global}>
-          <Button title="Button"/>
-          <Text> hell0o </Text>
-          {isLoading ? <ActivityIndicator/> : (
-            <FlatList
+      <PaperProvider theme={global}>
+        <View style={styles.view}>
+          <Text style={styles.title}> Recipes </Text>
+          <FlatList
               data={data}
               renderItem={({ item }) => (
-                <Text>{ item.recipe.calories }</Text>
+                <Text>{item.recipe.calories}</Text>
               )}
             />
-          )}
-      </ThemeProvider>
+        </View>
+      </PaperProvider>
     );
   }
 }
 
-
+const styles = StyleSheet.create({
+  view: {
+    ...view,
+    justifyContent:'center'
+  }, 
+  title: {
+    ...title
+  }
+})
