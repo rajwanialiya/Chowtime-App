7369d87ab0462cfed4a436b785032d296bc32ae3
Aliya Rajwani <rajwani.aliya@gmail.com>
2020-05-18 02:38:34 -0400

    flatlist + test carousel, noPadding style

--- components/recipes.js
+++ components/recipes.js
@@ -1,24 +1,18 @@
 import React, { useState } from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 
-import { StyleSheet, View, FlatList } from 'react-native';
-import { Text, ActivityIndicator, Chip } from 'react-native-paper';
+import { StyleSheet, View, FlatList, ScrollView, Dimensions, ImageBackground} from 'react-native';
+import { Text, ActivityIndicator } from 'react-native-paper';
 import Carousel from 'react-native-snap-carousel';
 
-//Theme
-import { global } from '../styles';
-
-//Styles
-import { view } from '../styles'
-import { title } from '../styles'
-import { subtitle } from '../styles'
-import { chip } from '../styles'
+//Styles & Theme
+import { global, noPadding, title, subtitle, chip } from '../styles'
 
 export function Recipes() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  const foodItems = ['chicken', 'tomato', 'apple', 'lemonsssssss', 'chicken', 'tomato', 'apple', 'lemonsssssss', 'chicken', 'tomato', 'apple', 'lemonsssssss'] // REPLACE AFTER
+  const foodItems = ['chicken', 'tomato', 'apple', 'tomato', 'apple'] // REPLACE AFTER
   const appKey = '4e12a9394efa795c901712637778f43c'
   const appID = 'd0721604'
   const base = 'https://api.edamam.com/search'
@@ -35,8 +29,8 @@ export function Recipes() {
       .then((json) => setData(json.hits))
       .catch((error) => console.error('oh no')) //figure out how to display error 
       .finally(() => setLoading(false));
-    
-      return (
+
+    return (
         <View style={styles.viewCenter}>
           <ActivityIndicator 
             color='#32CA81'
@@ -48,65 +42,104 @@ export function Recipes() {
     return (
       <PaperProvider theme={global}>
         <View style={styles.view}>
-          <Text style={styles.title}> Recipes </Text>
-          <Text style={styles.subtitle}> Your Ingredients </Text>
-          <FlatList
-            contentContainerStyle={{flexWrap:'wrap', flex: 0}}
-            style={styles.row}
-            horizontal={true}
-            scrollEnabled={false}
-            data={foodItems}
-            renderItem={({ item }) => (
-              <Text style={styles.chip}>{item}</Text>
-            )}
-          />
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Text style={styles.title}> Recipes </Text>
+            
+            {/* Your Ingredients */}
+            <Text style={styles.subtitle}> Your Ingredients </Text>
+            <FlatList
+              contentContainerStyle={{flexWrap:'wrap', flex: 0}}
+              style={styles.row}
+              horizontal={true}
+              scrollEnabled={false}
+              data={foodItems}
+              renderItem={({ item }) => (
+                <Text style={styles.chip}>{item}</Text>
+              )}
+            />
 
-          {/* Recipes */}
-          <Text style={styles.subtitle}> Recipes</Text>
-          <FlatList
-            data={data}
-            renderItem={({ item }) => (
-              <Text>{ item.recipe.calories }</Text>
-            )}
-        />
-          {/* <Carousel
+            {/* Recipes */}
+            <Text style={styles.subtitle}> Recipes</Text>
+            <View>
+              <FlatList
+                contentContainerStyle={{overflow:'scroll', paddingHorizontal:16}}
+                decelerationRate={0}
+                snapToInterval={Dimensions.get('window').width - 52 + 18} //your element width
+                snapToAlignment={"center"}
+                horizontal={true}
+                scrollEnabled={true}
+                data={data}
+                keyExtractor={item => item.recipe.label}
+                // renderItem={_renderItem}
+                renderItem={({ item }) => (
+                  <View style={{paddingRight:18}}>
+                    <ImageBackground
+                      style={{height:360, width:Dimensions.get('window').width - 52, borderRadius: 10, overflow:'hidden'}}
+                      source={{uri: item.recipe.image}}
+                      resizeMode='cover'
+                    />
+                    <Text style={styles.chip}>{item.recipe.calories}</Text>
+                  </View>
+                )}
+              />
+            </View>
+            {/* <Carousel
               layout={'default'}
-              // activeSlideOffset={2}
-              sliderWidth={200}
-              itemWidth={100}
-              inactiveSlideScale={0.98}
+              sliderWidth={Dimensions.get('window').width}
+              itemWidth={Dimensions.get('window').width - 32}
+              inactiveSlideScale={1}
+              slideStyle={{
+                paddingHorizontal: 8,
+              }}
 
               data={data}
-              renderItem={({ item }) => (
-                <Text>{item.recipe.calories}</Text>
-              )}
-            /> */}
+              keyExtractor={item => item.recipe.label}
+              renderItem={_renderItem}
+              /> */}
+          </ScrollView>
         </View>
       </PaperProvider>
     );
   }
 }
 
+// , shadowOffset:{  width: 10,  height: 10,  },
+//                 shadowColor: 'black',
+                // shadowOpacity: 1.0,
+
+function _renderItem({item,index}){
+  // const {height, width} = Dimensions.get('screen');
+  return (
+      <View>
+        {/* <Image style={{height: 350}} resizeMode='cover' source={{uri: item.recipe.image}}/> */}
+        <Text>{item.recipe.calories}</Text>
+      </View>
+  )
+}
+
 const styles = StyleSheet.create({
   view: {
-    ...view
+    ...noPadding
   }, 
   viewCenter: {
-    ...view,
+    ...noPadding,
     justifyContent: 'center'
   }, 
   title: {
-    ...title
+    ...title,
+    marginHorizontal: 16
   }, 
   subtitle: {
-    ...subtitle
+    ...subtitle,
+    marginHorizontal: 16
+  }, 
+  chip: {
+    ...chip
   }, 
   row: {
     flexDirection: 'column',
     flexGrow: 0,
     marginVertical: 8,
-  }, 
-  chip: {
-    ...chip
+    marginHorizontal: 16
   }
 })
