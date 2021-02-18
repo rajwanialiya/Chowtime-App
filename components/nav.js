d9f844e50024a35c7e4777c17c5c341c06432c7a
rajwanialiya <rajwani.aliya@gmail.com>
2021-02-17 23:15:20 -0500

    tensorflow camera

--- components/nav.js
+++ components/nav.js
@@ -8,6 +8,7 @@ import { createMaterialBottomTabNavigator } from "@react-navigation/material-bot
 
 //Components
 import CameraScreen from "./camera";
+import LiveCameraScreen from "./liveCameraScreen";
 import { RecipesTab } from "./recipes";
 import {SavedTab} from './savedRecipes'
 //Style
@@ -33,7 +34,7 @@ export function TabNav() {
       >
         <Tab.Screen
           name="Camera"
-          component={CameraScreen}
+          component={LiveCameraScreen}
 
           options={{
             tabBarLabel: "Camera",
